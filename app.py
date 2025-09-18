e0042439eb1c3816766578637a3a24c1c2747e80
rifai-ahmad11 <160980992+rifai-ahmad11@users.noreply.github.com>
2025-09-18 16:31:51 +0700

    Update app.py
    switch port to 8080

--- app.py
+++ app.py
@@ -147,7 +147,8 @@ if __name__ == '__main__':
     socketio.run(
         app,
         host='0.0.0.0',
-        port=5000,
+        port=8080,
         debug=True,
         allow_unsafe_werkzeug=True
     )
+
